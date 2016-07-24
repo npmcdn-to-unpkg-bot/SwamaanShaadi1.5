3669e472765e4dc605ac776a4a49fb6ec80ff040
Navendu <navenduk75@yahoo.com>
2016-07-24 10:36:18 -0400

    111

---
+++ SwamaanShaadi.DataLayer/Helper/ContextHelper.cs
@@ -0,0 +1,23 @@
+﻿using SwamaanShaadi.DomainClasses;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwamaanShaadi.DataLayer
+{
+    public static class ContextHelper
+    {
+        //Only use for short lived contexts
+        public static void ApplyStateChanges(this DbContext context)
+        {
+            foreach (var entry in context.ChangeTracker.Entries<IObjectWithState>())
+            {
+                IObjectWithState stateInfo = entry.Entity;
+                entry.State = StateHelper.ConvertState(stateInfo.State);
+            }
+        }
+    }
+}
--- SwamaanShaadi.DataLayer/Helper/StateHelper.cs
+++ SwamaanShaadi.DataLayer/Helper/StateHelper.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SwamaanShaadi.DataLayer.Helper
+namespace SwamaanShaadi.DataLayer
 {
     public class StateHelper
     {
